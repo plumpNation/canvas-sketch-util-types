83f87d855baf0ec8998aea7b839e59927f4f5565
Gavin King <gavin.king@cgi.com>
2023-01-11 10:50:33 +0100

    clamp is not required

--- math.d.ts
+++ math.d.ts
@@ -16,7 +16,7 @@ export function clamp01(v: any): any;
 export function smoothstep(min: any, max: any, t: any): number;
 export function damp(a: any, b: any, lambda: any, dt: any): number;
 export function dampArray(a: any, b: any, lambda: any, dt: any, out: any): any;
-export function mapRange(value: number, inputMin: number, inputMax: number, outputMin: number, outputMax: number, clamp: boolean): number;
+export function mapRange(value: number, inputMin: number, inputMax: number, outputMin: number, outputMax: number, clamp?: boolean): number;
 export declare function expand2D(p: any, defaultValue: any): any[];
 export declare function expand3D(p: any, defaultValue: any): any[];
 export declare function expand4D(p: any, defaultValue: any): any[];
